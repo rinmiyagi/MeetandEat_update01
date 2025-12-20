3771db2bbeea0c8901548a839964a2c78aaa4c2b
hatsune_kinoshita <hatsune.k.pc8010@gmail.com>
2025-12-19 23:02:11 -0800

    参加者側の画面に行くルート追加

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,12 +2,14 @@
 import { Route, Routes } from "react-router-dom";
 import AdminPage from "./pages/admin.tsx";
 import LP from "./pages/LandingPage.tsx";
+import Participant from "./pages/Participant.tsx";
 
 export default function App() {
   return (
     <Routes>
       <Route index element={<LP />} />
       <Route path="/admin" element={<AdminPage />} />
+      <Route path= "/participant" element = {<Participant />} />
     </Routes>
   );
 }
